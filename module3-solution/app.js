972887bd2fa79da486eca42936fcf0d0e5bfe811
Jirayuth Singngam <jirayuth.si.56@ubu.ac.th>
2016-11-15 13:09:26 +0700

    update solution3 no.2

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -10,28 +10,33 @@
 
 	function foundItemsDirective() {
 		return {
+			 restrict: "E",
 			templateUrl: 'menuItemList.html',
 			scope: {
-				foundItems: '<items',
+				foundItems: '<',
 				onRemove: '&'
 			}
 		};
 	}
-
-
+	
 	NarrowItDownController.$inject = ['MenuSearchService'];
-
 	function NarrowItDownController(MenuSearchService) {
 		var vm = this;
 		vm.itemList = [];
 		vm.searchTerm = "";
-
-		vm.getMenu = function () {
-			var count = 0;
-
+		vm.msg = false;
+		vm.doSearch = function () {
 			var promise = MenuSearchService.getMatchedMenuItems(vm.searchTerm);
 			promise.then(function (result) {
-				vm.itemList = result;
+				if (result.length != 0){
+					vm.itemList = result;
+					vm.msg = false;
+				}else{
+					vm.msg = true;
+				}
+					
+			}).catch(function (error) {
+				console.log(error);
 			});
 		}
 
@@ -53,28 +58,25 @@
 		service.getMatchedMenuItems = function (searchTerm) {
 
 			return $http({
-					method: "GET",
-					url: (ApiBasePath + "/menu_items.json"),
-				}).then(function (result) {
-					foundItems = [];
+				method: "GET",
+				url: (ApiBasePath + "/menu_items.json"),
+			}).then(function (result) {
+				
+				foundItems = [];
 
-					for (var i = 0; i < result.data.menu_items.length; i++) {
-						var description = result.data.menu_items[i].description;
-						console.log(description);
-						if (description.toLowerCase().indexOf(searchTerm) !== -1) {
-							
-							foundItems.push({
-								name: result.data.menu_items[i].name,
-								short_name: result.data.menu_items[i].short_name,
-								description: result.data.menu_items[i].description
-							});
-						}
+				for (var i = 0; i < result.data.menu_items.length; i++) {
+					var description = result.data.menu_items[i].description;
+					if (description.toLowerCase().indexOf(searchTerm) !== -1) {
+
+						foundItems.push({
+							name: result.data.menu_items[i].name,
+							short_name: result.data.menu_items[i].short_name,
+							description: result.data.menu_items[i].description
+						});
 					}
-					return foundItems;
-				})
-				.catch(function (error) {
-					console.log(error);
-				});
+				}
+				return foundItems;
+			});
 		}
 
 	}
